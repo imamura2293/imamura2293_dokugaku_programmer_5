3452ebc4b1e4e6c28cfefb61c950cc102c15cbc9
pana2 <imamura2293@gmail.com>
2020-09-06 21:41:41 +0900

    dai5syou

--- dokugaku5.py
+++ dokugaku5.py
@@ -50,4 +50,98 @@ colors1 + colors2
 colors = ["blue", "green", "yellow"]
 "green" in colors
 
+colors = ["blue", "green", "yellow"]
+"black" not in colors
 
+len(colors)
+
+colors = ["purple", "orange", "green"]
+
+guess = input("何色でしょう?(入力してください):")
+
+if guess in colors:
+    print("当たり!")
+else:
+    print("ハズレ!また挑戦してね。")
+
+my_tuple = tuple()
+my_tuple
+
+my_tuple - ()
+my_tuple
+
+rndm = ("M. Jackson", 1950, True)
+
+# これはタプルです
+("self_taught",)
+
+# これはタプルではなく数値演算に使うカッコです
+(9) + 1
+
+dys = ("1984", "Brave New World", "Fahrenheit 451")
+dys[1] = "Handmaid's Tale"
+
+dys = ("1984", "Brave New World", "Fahrenheit 451")
+dys[2]
+
+dys = ("1984", "Brave New World", "Fahrenheit 451")
+"Handmaid's Tale" not in dys
+
+my_dict = dict()
+my_dic
+
+my_dict = {}
+my_dict
+
+fruits = {"Apple": "Red", "Banana": "Yellow"}
+
+fruits
+
+facts = dict()
+
+# バリューを追加
+facts["code"] = "fun"
+# キーで参照
+facts["code"]
+
+# バリューを追加
+facts["Bill"] = "Gates"
+# キーで参照
+facts["Bill"]
+
+# バリューを追加
+facts["founded"] = 1776
+# キーで参照
+facts("founded")
+
+bill = {"Bill Gates": "charitable"}
+"Bill Gates" in bill
+
+bill = {"Bill Gates": "charitable"}
+"Bill Doors" not in bill
+
+
+
+
+
+books = { "Dracula": "stoker",
+"1984": "Orwell",
+"The Trial": "Kafka"}
+
+del books ("The Trial"]
+
+books
+
+songs = {"1": "fun",
+    "2": "blue",
+    "3": "me",
+    "4": "floor",
+    "5": "live"
+
+ = input("数字を入力してください:") rooms)
+if n in songs:
+    song = songs (n)
+
+    print (song)
+else:
+    print("見つかりません。")
